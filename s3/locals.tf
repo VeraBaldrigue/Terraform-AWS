1a4f7ac70b6f00ba8ddcea00907b0571e4e98976
Vera Baldrigue <123998237+VeraBaldrigue@users.noreply.github.com>
2023-03-05 18:34:32 -0300

    Deletando diretório S3

--- s3/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  # Common tags to be assigned to all resources
-  common_tags = {
-    locals-var1 = "Curso-Terraform-locals"
-    locals-var2 = "Vera-Baldrigue"
-  }
-  dir    = "C:/Vera/Terraform/"
-  fileup = "arquivo_teste_upload_s3.json"
-}
